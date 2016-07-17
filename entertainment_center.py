bb60e406bfef2774a48cd0634dbc63d8ab84e44d
Ashutosh Purushottam <apurushottam@Ashutoshs-MacBook-Pro.local>
2016-07-17 12:46:03 +0530

    change entertainment center code and delete read_response

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,43 +1,99 @@
+# Imports
+import tmdbsimple as tmdb
+tmdb.API_KEY = '16e06503367744093ba784c46dc0fc9c'
 import media
 import fresh_tomatoes
 
+# URLs for trailer and posters for movie objects
+YOUTUBE_BASE_URL = 'https://www.youtube.com/watch?v='
+TMDB_POSTER_BASE_URL = 'http://image.tmdb.org/t/p/w500/'
 
 # Create instance of six favorite movies
 
-toy_story = media.Movie("Toy Story",
-    "A story of a boy and his toys that come to life",
-    "http://cdn.collider.com/wp-content/uploads/toy-story-poster1.jpg",
-    "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+# Construct avatar trailer url
+avatar_videos = tmdb.Movies(19995).videos()
+avatar_video = avatar_videos['results'][0]
+avatar_trailer_url = YOUTUBE_BASE_URL +  avatar_video['key']
 
-avatar = media.Movie("Avatar",
-    "A marine on an alien planet",
-    "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
-    "https://www.youtube.com/watch?v=cRdxXPV9GNQ")
+# Construct title, overview and poster for avatar
+avatar_info = tmdb.Movies(19995).info()
+avatar_title = avatar_info['title']
+avatar_overview = avatar_info['overview']
+avatar_poster_url = TMDB_POSTER_BASE_URL + avatar_info['poster_path']
 
-interstellar = media.Movie("Interstellar",
-    "A team of explorers travel through a wormhole in space in an attempt to ensure humanity's survival",
-    "http://www.hollywoodreporter.com/sites/default/files/custom/Blog_Images/interstellar3.jpg",
-    "https://www.youtube.com/watch?v=zSWdZVtXT7E")
+# Construct avatar movie object
+avatar = media.Movie(avatar_title, avatar_overview, avatar_poster_url, avatar_trailer_url)
 
-gone_girl = media.Movie("Gone Girl",
-    "With his wife's disappearance having become the focus of an intense media circus,"
-    "a man sees the spotlight turned on him when it's suspected that he may not be innocent",
-    "http://sheilas.org.au/wp-content/uploads/2014/10/Gone-Girl-poster-31.jpg",
-    "https://www.youtube.com/watch?v=Ym3LB0lOJ0o")
+# Construct creed trailer url
+creed_videos = tmdb.Movies(312221).videos()
+creed_video = creed_videos['results'][0]
+creed_trailer_url = YOUTUBE_BASE_URL + creed_video['key']
 
-her = media.Movie("Her",
-    "A lonely writer develops an unlikely relationship with his newly purchased operating system"
-    "that's designed to meet his every need",
-    "https://shyfyy.files.wordpress.com/2013/08/her1.jpg",
-    "https://www.youtube.com/watch?v=WzV6mXIOVl4")
+# Construct title, overview and poster for creed
+creed_info = tmdb.Movies(312221).info()
+creed_title = creed_info['title']
+creed_overview = creed_info['overview']
+creed_poster_url = TMDB_POSTER_BASE_URL + creed_info['poster_path']
+
+# Create creed object
+creed = media.Movie(creed_title, creed_overview, creed_poster_url, creed_trailer_url)
+
+# Construct interstellar trailer url
+interstellar_videos = tmdb.Movies(157336).videos()
+interstellar_video = interstellar_videos['results'][0]
+interstellar_trailer_url = YOUTUBE_BASE_URL +  interstellar_video['key']
+
+# Construct title, overview and poster for interstellar
+interstellar_info = tmdb.Movies(157336).info()
+interstellar_title = interstellar_info['title']
+interstellar_overview = interstellar_info['overview']
+interstellar_poster_url = TMDB_POSTER_BASE_URL + interstellar_info['poster_path']
+
+# Construct interstellar movie object
+interstellar = media.Movie(interstellar_title, interstellar_overview, interstellar_poster_url, interstellar_trailer_url)
+
+# Construct titanic trailer url
+titanic_videos = tmdb.Movies(597).videos()
+titanic_video = titanic_videos['results'][0]
+titanic_trailer_url = YOUTUBE_BASE_URL +  titanic_video['key']
+
+# Construct title, overview and poster for titanic
+titanic_info = tmdb.Movies(597).info()
+titanic_title = titanic_info['title']
+titanic_overview = titanic_info['overview']
+titanic_poster_url = TMDB_POSTER_BASE_URL + titanic_info['poster_path']
+
+# Construct titanic movie object
+titanic = media.Movie(titanic_title, titanic_overview, titanic_poster_url, titanic_trailer_url)
+
+# Construct con air trailer url
+conair_videos = tmdb.Movies(1701).videos()
+conair_video = conair_videos['results'][0]
+conair_trailer_url = YOUTUBE_BASE_URL +  conair_video['key']
+
+# Construct title, overview and poster for con air
+conair_info = tmdb.Movies(1701).info()
+conair_title = conair_info['title']
+conair_overview = conair_info['overview']
+conair_poster_url = TMDB_POSTER_BASE_URL + conair_info['poster_path']
+
+# Construct con air movie object
+conair = media.Movie(conair_title, conair_overview, conair_poster_url, conair_trailer_url)
+
+# Construct insidious trailer url
+insidious_videos = tmdb.Movies(49018).videos()
+insidious_video = insidious_videos['results'][0]
+insidious_trailer_url = YOUTUBE_BASE_URL +  insidious_video['key']
+
+# Construct title, overview and poster for insidiuos
+insidious_info = tmdb.Movies(49018).info()
+insidious_title = insidious_info['title']
+insidious_overview = insidious_info['overview']
+insidious_poster_url = TMDB_POSTER_BASE_URL + insidious_info['poster_path']
+
+# Construct insidious movie object
+insidious = media.Movie(insidious_title, insidious_overview, insidious_poster_url, insidious_trailer_url)
 
 
-creed = media.Movie("Creed",
-    "The former World Heavyweight Champion Rocky Balboa serves as a trainer and"
-    "mentor to Adonis Johnson, the son of his late friend and former rival Apollo Creed",
-    "http://cdn2-www.comingsoon.net/assets/uploads/gallery/creed/creedpostersmall.jpg",
-    "https://www.youtube.com/watch?v=Uv554B7YHk4")
-
-
-movies = [toy_story, avatar, interstellar, gone_girl, her, creed]
+movies = [avatar, titanic, creed, interstellar, conair, insidious]
 fresh_tomatoes.open_movies_page(movies)
